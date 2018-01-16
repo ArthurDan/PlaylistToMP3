3e7fdf163b256b1482e1c03f54e43a7c0e020114
damienhervieu <damien.hervieu@ynov.com>
2018-01-16 10:10:57 +0100

    Post route added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'YTPlaylistToMP3' });
 });
 
+router.post('/', function(req, res){
+	console.log(req.body.youtubeURI);
+});
+
 module.exports = router;
