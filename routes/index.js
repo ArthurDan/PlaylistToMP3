5e609f0fea86920144fa7b8e56cb6693d0b4f271
BastienBellariva <bastien.bellariva@ynov.com>
2018-01-17 19:15:25 +0100

    Premier test add list_url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function test(req, res, next) {
+  console.log("Je suis ici caca");
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'YTPlaylistToMP3' });
+});
+
+//Test bastien lister tous les liens d'une playlist
+router.get('/list_url', function(req, res) {
+  list_url.test(auth, requestData);
+  res.render('index', { title: 'Liste des URL' });
 });
 
 module.exports = router;
