faed1cf57370504bfcbf8263b181975171f4e084
damienhervieu <damien.hervieu@ynov.com>
2018-02-05 23:04:47 +0100

    Youtube video conversion working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-var fs = require('fs');
 const express = require('express');
 const router = express.Router();
-var downloader = require('./downloader');
-var dl = new downloader;
+const horizon = require('horizon-youtube-mp3');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -14,13 +12,30 @@ router.post('/', function(req, res){
 	var uri = req.body.youtubeURI;
 	var splitUri = uri.split('=');
 	var videoId = splitUri[1];
-	dl.getMP3({videoId: "9fWxCIi5PIw", name: "Rauuul Gillette"}, function(err,res){
-    if(err)
-        throw err;
-    else{
-        console.log("Song "+ i + " was downloaded: " + res.file);
-    }
+	horizon.downloadToLocal(
+		uri,
+		'C:/Users/falle/Music/yt2mp3',
+		null,
+		null,
+		null,
+		onConvertVideoComplete,
+		onConvertVideoProgress
+	);
 	res.render('download', { title: 'Downloading...' });
 });
+
+function onConvertVideoComplete(err, result) {
+  console.log(err, result);
+  // Will return...
+  //null, conversionFileComplete
+}
+
+function onConvertVideoProgress(percent, timemark, targetSize) {
+  console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
+  // Will return...
+  // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
+  // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
+  // Progress: 100.0083970106642 Timemark: 00:02:34.83 Target Size: 2420
+}
 
 module.exports = router;
