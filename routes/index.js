e5bdb46d0b689f680d144c14b812102ed75a5c4f
Whiteyeas <gabriel-le-jedi@hotmail.fr>
2018-02-06 03:45:30 +0100

    dodo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const horizon = require('horizon-youtube-mp3');
+const list = require('./playlist.js');
 
 router.use(function test(req, res, next) {
-  console.log("Je suis ici caca");
   next();
 });
 
@@ -23,31 +23,9 @@ router.post('/', function(req, res){
 	var uri = req.body.youtubeURI;
 	var splitUri = uri.split('=');
 	var videoId = splitUri[1];
-	horizon.downloadToLocal(
-		uri,
-		'D:/Users/Whiteyeas/FreeMusic',
-		null,
-		null,
-		null,
-		onConvertVideoComplete,
-		onConvertVideoProgress
-	);
+	list(videoId);
+
 	res.render('download', { title: 'Downloading...' });
 });
 
-function onConvertVideoComplete(err, result) {
-  console.log(err, result);
-  // Will return...
-  //null, conversionFileComplete
-}
-
-function onConvertVideoProgress(percent, timemark, targetSize) {
-  console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
-  // Will return...
-  // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
-  // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
-  // Progress: 100.0083970106642 Timemark: 00:02:34.83 Target Size: 2420
-}
-
->>>>>>> youtube-to-mp3
 module.exports = router;
